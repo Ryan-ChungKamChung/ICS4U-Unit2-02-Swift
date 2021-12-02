74393178567aa6d14b9ba157641e984ef8b56f2e
Ubuntu <ubuntu@ip-172-31-92-172.ec2.internal>
2021-12-02 03:29:20 +0000

    added Factorial.swift

---
+++ Factorial.swift
@@ -0,0 +1,37 @@
+//
+//  ReverseString.swift
+//
+//  Created by Ryan Chung
+//  Created on 2021-12-01
+//  Version 1.0
+//  Copyright (c) 2021 Ryan Chung. All rights reserved.
+//
+//  This program reverses a user-inputted string.
+//
+
+import Foundation
+
+// Returns a factorial via recursion
+func factorial(input: Int) -> Int {
+
+    if input == 0 {
+        return 1
+    } else {
+        return input * factorial(input: input - 1)
+    }
+}
+
+// Captures user input, feeds it to factorial() and outputs the reversed string
+print("Enter an integer: ", terminator: "")
+
+guard let inputString = readLine(), !inputString.isEmpty else {
+    print("Please enter an integer.")
+    exit(001)
+}
+
+if let input = Int(inputString) {
+    let factorialValue = factorial(input: input)
+    print(factorialValue)
+}
+
+print("\nDone.")
